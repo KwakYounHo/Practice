bba146d353c5e14414257840d070e6d1500316a4
KwakYounHo <kwakyh951@gmail.com>
2023-02-23 18:17:47 +0900

    23.02.23 계산기의 숫자 박스에 숫자를 문자열로 표기하는 법?

---
+++ js_prac/calculator_mix/test.js
@@ -0,0 +1,43 @@
+let box = [];
+
+let a = 1;
+let b = 2;
+
+box.push(a);
+console.log(box.length);
+
+/*
+box = [1];
+= 1
+*/
+
+// -------------------------------------------------
+
+let test = [];
+
+// 1 클릭
+for (i=0; i<1; i++) {
+  box.push[i];
+  console.log(box.length);
+  for (i=0; i<box.length;) {
+    box.pop()
+  }
+}
+
+//2 클릭
+for (i=0; i<2; i++) {
+  box.push[i];
+  console.log(box.length);
+  for (i=0; i<box.length;) {
+    box.pop()
+  }
+}
+
+// 3 클릭
+for (i=0; i<3; i++) {
+  box.push[i];
+  console.log(box.length);
+  for (i=0; i<box.length;) {
+    box.pop()
+  }
+}
